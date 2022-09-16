003193c6a89ca27b6077a28bf8504c30f44945b9
SheikoAndrei <113633630+SheikoAndrei@users.noreply.github.com>
2022-09-16 10:00:04 +0300

    Добавил комментарии
    Уменьшил код PHP;

--- index.php
+++ index.php
@@ -1,78 +1,104 @@
 <?php
 error_reporting(1);
+/* пригодится ниже */
 $time1 = microtime(true);
 $date_time = date("Y-m-d H:i:s");
 
+/* константы для удобства */
 if (strpos($_SERVER['SCRIPT_FILENAME'],'/')===0 || strpos($_SERVER['SCRIPT_FILENAME'],'/')>=1) { define('DS', '/'); } else { define('DS', '\\'); }
 $path_arr = explode(DS,$_SERVER['SCRIPT_FILENAME']);
 $sfn = array_pop($path_arr);
 define('HOST_URL', $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST']);
 define('SITE_URL', HOST_URL . str_replace($sfn,'',$_SERVER['SCRIPT_NAME']));
 define('SITE_PATH', join(DS,$path_arr));
-define('LOG_URL', SITE_URL.'x-log');
-define('LOG_PATH', SITE_PATH.DS.'x-log');
-define('DB_URL', SITE_URL.'x-db');
-define('DB_PATH', SITE_PATH.DS.'x-db');
-if (!is_dir(LOG_PATH)) { mkdir(LOG_PATH); }
-if (!is_dir(DB_PATH)) { mkdir(DB_PATH); }
+define('LOG_URL', SITE_URL.'log.txt');
+define('LOG_PATH', SITE_PATH.DS.'log.txt');
+define('DB_URL', SITE_URL.'users.json');
+define('DB_PATH', SITE_PATH.DS.'users.json');
 
+/* пришла наша форма */
 if ($_POST['id']=='ok')
 	{
-	sleep(2);
-	$db = json_decode(file_get_contents(DB_PATH.DS.'users.json'),true);
+	/* чтобы заметить спинер на странице */
+	sleep(1);
+	/* на всякий пожарный */
 	$p = $_POST;
+	/* будем считать, что это база */
+	$db = json_decode(file_get_contents(DB_PATH),true);
+	/* определим массив на выдачу чтобы избежать null-ов */
 	$json = array(
 		'ans'=>'',
 		'class_alert'=>'',
 		'view_text'=>array()
 		);
+	/* валидируем email (лучше положится на стандарты) */
 	if (!filter_var($p['email'], FILTER_VALIDATE_EMAIL))
 		{
 		$msg = 'email error';
 		$json['view_text'][] = '<p>- The email address is not correct.</p>';
 		}
+	/* проверяем идентичность паролей */
 	if ($p['password']!=$p['passwordr'])
 		{
 		$msg = 'pass error';
 		$json['view_text'][] = '<p>- The second password has not passed the test for identity.</p>';
 		}
+	/* второй пароль уже не нужен, мы все поняли */
+	unset($p['passwordr']);
+	/* маст хев */
+	$p['password'] = md5($p['password']);
+	/* лучше представить email как PRIMARY KEY чем перебирать весь массив (для выбранного варианта базы) */
 	if ($db[$p['email']])
 		{
 		$msg = 'email duble';
 		$json['view_text'][] = '<p>- This email has already been registered.</p>';
 		}
+	/* если что-то пришло не то - выдать ахтунг */
 	if (count($json['view_text'])>=1)
 		{
 		$json['ans'] = 'warning';
-		$json['class_alert'] = 'alert alert-warning';
+		$json['class_alert'] = 'alert-warning';
 		array_unshift($json['view_text'],'<h5>WARNING!</h5>');
 		}
+	/* теоретически все норм: сохранить в базу и выдать окей. тут подробнее */
 	else
 		{
 		$msg = 'user add';
+		/* добавил тайминг для "а если мы ..." */
 		$p['time_change'] = $p['time_add'] = $date_time;
+		/* присвоим id - поскольку мы его не используем для идентификации, можно просто порядковый номер */
 		$p['id'] = number_format(count($db)+1,0,'','');
-		$p['password'] = md5($p['password']);
+		/* наш PRIMARY KEY */
 		$db[$p['email']] = $p;
-		file_put_contents(DB_PATH.DS.'users.json',json_encode($db,JSON_UNESCAPED_UNICODE));
+		/* сохраняем без проверки разрешений на запись, подразумеваем, что можно */
+		file_put_contents(DB_PATH,json_encode($db,JSON_UNESCAPED_UNICODE));
+		/* ну и наш ответ Чемберлену */
 		$json['ans'] = 'useradd';
-		$json['class_alert'] = 'alert alert-success';
+		$json['class_alert'] = 'alert-success';
 		$json['view_text'] = array(
 			'<h5>CONGRATULATIONS!</h5>',
 			'<p>- The new user is added.</p>'
 			);
 		}
-	addLog($time1,$date_time,$p,$msg);
+	/* логируем */
+	file_put_contents(LOG_PATH,$time1."\t".$date_time."\t".$msg."\t".json_encode($p,JSON_UNESCAPED_UNICODE)."\n",FILE_APPEND);
+	/* выводим результат тяжкого труда */
 	header('Content-Type: application/json; charset=utf-8');
 	echo json_encode($json);
 	die;
 	}
-
+else if ($_POST || $_GET)
+	{
+	/* всех шаромыжников, что лезут мимо кассы - посылаем на главную страницу */
+	header('Location: '.SITE_URL);
+	die;
+	}
+/* HTML можно было проинклюдить, но так быстрее будет работать */
 ?>
 <!doctype html>
 <html lang="ru">
 	<head>
-		<title>Test</title>
+		<title>regForm</title>
 		<meta name="description" content="">
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -90,7 +116,7 @@ if ($_POST['id']=='ok')
 				<div class="container">
 					<a href="<?php echo SITE_URL; ?>" class="navbar-brand">
 						<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-square" viewBox="0 0 20 20"><path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z"/></svg>
-						<strong>Test</strong>
+						<strong>regForm</strong>
 					</a>
 					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
 					<div class="collapse navbar-collapse" id="navbarCollapse">
@@ -100,7 +126,9 @@ if ($_POST['id']=='ok')
 							</li>
 						</ul>
 						<div class="d-flex">
-						<button class="btn btn-light" type="button" data-bs-toggle="modal" data-bs-target="#modalform">Sign in</button>
+							<div class="btn-group" role="group" aria-label="">
+								<button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#modalform">Sign in</button>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -111,7 +139,7 @@ if ($_POST['id']=='ok')
 			<div class="container col-lg-3 mx-auto">
 				<h1>Marcus Tullius Cicero</h1>
 				<img class="rounded mx-auto d-block my-4" alt="Marcus Tullius Cicero" src="//upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Bust_of_Cicero_%281st-cent._BC%29_-_Palazzo_Nuovo_-_Musei_Capitolini_-_Rome_2016.jpg/200px-Bust_of_Cicero_%281st-cent._BC%29_-_Palazzo_Nuovo_-_Musei_Capitolini_-_Rome_2016.jpg" decoding="async" width="200" height="300" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Bust_of_Cicero_%281st-cent._BC%29_-_Palazzo_Nuovo_-_Musei_Capitolini_-_Rome_2016.jpg/300px-Bust_of_Cicero_%281st-cent._BC%29_-_Palazzo_Nuovo_-_Musei_Capitolini_-_Rome_2016.jpg 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Bust_of_Cicero_%281st-cent._BC%29_-_Palazzo_Nuovo_-_Musei_Capitolini_-_Rome_2016.jpg/400px-Bust_of_Cicero_%281st-cent._BC%29_-_Palazzo_Nuovo_-_Musei_Capitolini_-_Rome_2016.jpg 2x">
-				<div class="mb-4 text-center"><button class="btn btn-success" type="button" data-bs-toggle="modal" data-bs-target="#modalform">Sign in</button></div>
+				<div class="mb-4 text-center"><button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#modalform">Sign in</button></div>
 				<p>Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui do<b>lorem ipsum</b>, quia <b>dolor sit, amet, consectetur, adipisci</b> v<b>elit, sed</b> quia non numquam <b>eius mod</b>i <b>tempor</b>a <b>incidunt, ut labore et dolore magna</b>m <b>aliqua</b>m quaerat voluptatem. <b>Ut enim ad minim</b>a <b>veniam, quis nostru</b>m <b>exercitation</b>em <b>ullam co</b>rporis suscipit<b> labori</b>o<b>s</b>am, <b>nisi ut aliquid ex ea commod</b>i <b>consequat</b>ur? <b>Quis aute</b>m vel eum <b>iure reprehenderit,</b> qui <b>in</b> ea <b>voluptate velit esse</b>, quam nihil molestiae <b>c</b>onsequatur, vel <b>illum</b>, qui <b>dolore</b>m <b>eu</b>m <b>fugiat</b>, quo voluptas <b>nulla pariatur</b>? At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias <b>exceptur</b>i <b>sint, obcaecat</b>i <b>cupiditat</b>e <b>non pro</b>v<b>ident</b>, similique <b>sunt in culpa</b>, <b>qui officia deserunt mollit</b>ia <b>anim</b>i, <b>id est laborum</b> et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
 				<div class="text-end mt-2">
 					<pre>load: <?php echo number_format(microtime(true)-$time1,6,'.',''); ?> s.</pre>
@@ -150,6 +178,7 @@ if ($_POST['id']=='ok')
 							<div class="input-group mb-3">
 								<input type="password" class="form-control" name="password" id="password" placeholder="Password" aria-label="Password" aria-describedby="Password" required>
 								<span class="input-group-text">Password</span>
+								<button type="button" class="input-group-text btn btn-success" id="password_random">Gen</button>
 								<div class="invalid-feedback">Please provide a valid Password.</div>
 							</div>
 							<div class="input-group mb-3">
@@ -163,7 +192,7 @@ if ($_POST['id']=='ok')
 							</div>
 							<div class="form-check">
 								<input class="form-check-input" type="checkbox" id="email_required">
-								<label class="form-check-label" for="email_required">Check the email of PHP</label>
+								<label class="form-check-label" for="email_required">Check email of PHP</label>
 							</div>
 						</div>
 						<div class="modal-footer">
@@ -171,13 +200,13 @@ if ($_POST['id']=='ok')
 							<button type="submit" class="btn btn-primary" id="signin">Sign in</button>
 						</div>
 					</form>
-					<div class="align-self-center rounded" id="roller">
+					<div class="align-self-center rounded fade" id="roller">
 						<div id="roller_box" class="align-middle text-center">
 							<div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
 						</div>
 					</div>
-					<div class="align-self-center rounded" id="message">
-						<div id="message_box" class="align-middle text-center" role="alert"></div>
+					<div class="align-self-center rounded fade" id="message">
+						<div id="message_box" class="align-middle text-center alert" role="alert"></div>
 					</div>
 				</div>
 			</div>
@@ -188,13 +217,3 @@ if ($_POST['id']=='ok')
 
 	</body>
 </html>
-<?
-
-function addLog($time1,$date_time,$p,$msg)
-	{
-	unset($p['password']);
-	unset($p['passwordr']);
-	file_put_contents(LOG_PATH.DS.'log.txt',$time1."\t".$date_time."\t".$msg."\t".json_encode($p,JSON_UNESCAPED_UNICODE)."\n",FILE_APPEND);
-	}
-
-?>
--- tmpl/js/bootstrap.add.js
+++ tmpl/js/bootstrap.add.js
@@ -3,61 +3,103 @@
 	var forms = document.querySelectorAll('.needs-validation')
 	Array.prototype.slice.call(forms)
 		.forEach(function (form) {
-			form.addEventListener('submit', function (e) {
-				e.preventDefault();
-				if (document.getElementById('password_view').checked==true) {
-					document.getElementById('password_view').click();
+			form.addEventListener('submit', function (elem) {
+				elem.preventDefault();
+				form.classList.add('was-validated');
+				var timeout = 3000;
+				var password_view = document.getElementById('password_view');
+				var roller = document.getElementById('roller');
+				var message = document.getElementById('message');
+				var message_box = document.getElementById('message_box');
+
+
+
+
+				if (password_view.checked==true) {
+					password_view.click();
 				}
 				if (!form.checkValidity()) {
-					e.stopPropagation();
+					elem.stopPropagation();
 				}
-				form.classList.add('was-validated');
-				if (form.checkValidity()) {
-					var r = document.getElementById('roller');
-					var message = document.getElementById('message');
-					var message_box = document.getElementById('message_box');
-					if (r.style.visibility == 'hidden' || r.style.visibility == '') { r.style.visibility = 'visible' } else { r.style.visibility = 'hidden'; }
+				else {
+					roller.style.visibility = 'visible';
+					roller.classList.add('show');
+
 					fetch('index.php', {method: 'POST',body: new FormData( document.getElementById('form') )})
 					.then(response=>response.json())
 					.then(data=>{
-						r.style.visibility = 'hidden';
+						roller.style.visibility = 'hidden';
+						roller.classList.remove('show');
 						message.style.visibility = 'visible';
+						message.classList.add('show');
+						setTimeout(() => {
+							message.style.visibility = 'hidden';
+							message.classList.remove('show');
+							message_box.classList.remove('alert-warning');
+							message_box.classList.remove('alert-success');
+							}, timeout);
+
 						if (data['ans']=='warning') {
-							message_box.setAttribute('class','align-middle '+data['class_alert']);
+							message_box.classList.add(data['class_alert']);
 							message_box.innerHTML = data['view_text'].join('');
-							setTimeout(() => { message.style.visibility = 'hidden'; }, 2000);
+							document.getElementById('email').classList.add('is-invalid');
+							
 						}
 						else if (data['ans']=='useradd') {
-							document.getElementById('form').setAttribute('class','needs-validation');
-							message_box.setAttribute('class','align-middle '+data['class_alert']);
+							document.getElementById('email').classList.remove('is-invalid');
+							form.classList.remove('was-validated');
+							message_box.classList.add(data['class_alert']);
 							message_box.innerHTML = data['view_text'].join('');
 							setTimeout(() => {
-								document.getElementById('tclose').click(); message.style.visibility = 'hidden';
+								document.getElementById('tclose').click();
 								var inp = document.querySelectorAll('input');
-								for (var i=0; i<inp.length; i++) {
-									if (inp[i].getAttribute('aria-describedby')) { inp[i].value = ''; }
-								}
-							}, 2000);
+								for (var i=0; i<inp.length; i++) { if (inp[i].getAttribute('aria-describedby')) { inp[i].value = ''; } }
+							}, timeout);
 						}
 						else {
-							message_box.setAttribute('class','align-middle alert alert-warning');
+							message_box.classList.add('alert-warning');
 							message_box.innerHTML = 'What a fuck one!';
-							setTimeout(() => { document.getElementById('tclose').click(); message.style.visibility = 'hidden'; }, 2000);
+							setTimeout(() => {
+								document.getElementById('tclose').click();
+								}, timeout);
 							}
-						console.log(data);
 					})
 					.catch(()=>{
-						r.style.visibility = 'hidden';
+						roller.style.visibility = 'hidden';
+						roller.classList.remove('show');
 						message.style.visibility = 'visible';
-						message_box.setAttribute('class','align-middle alert alert-danger');
+						message.classList.add('show');
+						message_box.classList.add('alert-danger');
 						message_box.innerHTML = 'What a fuck two!';
-						setTimeout(() => { document.getElementById('tclose').click(); message.style.visibility = 'hidden'; }, 2000);
+						setTimeout(() => {
+							document.getElementById('tclose').click();
+							message.style.visibility = 'hidden';
+							message.classList.remove('show');
+							}, timeout);
 					});
 				}
 			}, false)
 		})
 })()
 
+document.getElementById('password_random').addEventListener('click',()=>{
+	var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()+-_';
+	var password = '';
+	for (var i=0; i<=12; i++) {
+		var rand = Math.floor(Math.random() * chars.length);
+		password += chars.substring(rand, rand+1);
+		}
+	var idp1 = document.getElementById('password');
+	var idp2 = document.getElementById('passwordr');
+	idp1.value = password;
+	idp2.value = password;
+	if (document.getElementById('password_view').checked==false)
+		{
+		document.getElementById('password_view').checked = true;
+		idp1.setAttribute('type','text');
+		idp2.setAttribute('type','text');
+		}
+	});
 
 document.getElementById('password_view').addEventListener('click',()=>{
 	var idp1 = document.getElementById('password');
@@ -79,4 +121,11 @@ document.getElementById('email_required').addEventListener('click',()=>{
 		ider.removeAttribute('required');
 	}
 	});
-
+/*
+document.getElementById('message').addEventListener('click',()=>{
+	document.getElementById('message').style.visibility = 'hidden';
+	document.getElementById('message').classList.remove('show');
+	document.getElementById('message_box').classList.remove('alert-warning');
+	document.getElementById('message_box').classList.remove('alert-success');
+	});
+*/
